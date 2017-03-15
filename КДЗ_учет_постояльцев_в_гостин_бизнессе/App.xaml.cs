472fa10e0eeb78fd96b85f8234c41fd4f4288e21
BorMC <borisenko-evgenii@mail.ru>
2017-03-15 18:48:53 +0300

    Добавьте файлы проекта.

---
+++ КДЗ_учет_постояльцев_в_гостин_бизнессе/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace КДЗ_учет_постояльцев_в_гостин_бизнессе
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
